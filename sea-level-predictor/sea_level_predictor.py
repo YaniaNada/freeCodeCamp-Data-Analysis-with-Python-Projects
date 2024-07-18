855d4c46bd4c28b5b55a14efb3733d253dce122d
YaniaNada <nadayania@gmail.com>
2024-07-18 12:45:45 +0200

    sol_update

--- sea-level-predictor/sea_level_predictor.py
+++ sea-level-predictor/sea_level_predictor.py
@@ -1,22 +1,54 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
-
+    df = pd.read_csv('sea-level-predictor/epa-sea-level.csv')
+    
     # Create scatter plot
-
+    plt.figure(figsize= (14,6))
+    plt.scatter(data=df, x='Year', y='CSIRO Adjusted Sea Level')
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title("Rise in Sea Level")
 
     # Create first line of best fit
+    result= linregress(df['Year'],df['CSIRO Adjusted Sea Level'])
+    slope= result.slope
+    intercept= result.intercept
 
+    future_years = np.arange(1880, 2051)
+    predicted_sea_level_rise = slope * future_years + intercept
+
+    plt.figure(figsize= (14,6))
+    plt.scatter(data=df, x='Year', y='CSIRO Adjusted Sea Level', color='blue', label='Observed data')
+    plt.plot(future_years, predicted_sea_level_rise, color= 'red', label='Line of best fit')
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title("Rise in Sea Level")    
 
     # Create second line of best fit
+    filtered_df= df[df['Year']>= 2000]
+    filtered_years = filtered_df['Year']
+    filtered_sea_level_rise = filtered_df['CSIRO Adjusted Sea Level']
 
+    result= linregress(filtered_years,filtered_sea_level_rise)
+    slope= result.slope
+    intercept= result.intercept
+
+    future_years= np.arange(2000, 2051)
+
+    predicted_sea_level_rise= slope*future_years + intercept
+
+    plt.scatter(filtered_df['Year'], filtered_df['CSIRO Adjusted Sea Level'], color='blue', label='Observed data')
+    plt.plot(future_years, predicted_sea_level_rise, color='green', label='Second line of best fit (2000 onwards)')
 
     # Add labels and title
-
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title("Rise in Sea Level")
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
